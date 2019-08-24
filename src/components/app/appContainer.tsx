edf81e319f8992cf3ed1200c21b06f33f27fc7cb
jaehyoungyoo <kenshinhm@naver.com>
2019-08-24 13:07:31 +0900

    typescript and global styles

--- src/components/app/appContainer.tsx
+++ src/components/app/appContainer.tsx
@@ -1,8 +1,18 @@
 import React from "react";
-import {graphql} from "react-apollo";
+import { graphql } from "react-apollo";
+import GlobalStyle from "../../globalStyle";
+import theme from "../../theme";
+import { ThemeProvider } from "../../typed-components";
 import AppPresenter from "./appPresenter";
-import {IS_LOGGED_IN} from "./appQueries";
+import { IS_LOGGED_IN } from "./appQueries";
 
-const AppContainer = ({data}) => <AppPresenter isLoggedIn={data.isLoggedIn}/>;
+const AppContainer = ({ data }) => (
+    <ThemeProvider theme={theme}>
+        <React.Fragment>
+            <AppPresenter isLoggedIn={data.isLoggedIn} />
+            <GlobalStyle />
+        </React.Fragment>
+    </ThemeProvider>
+);
 
 export default graphql(IS_LOGGED_IN)(AppContainer);
